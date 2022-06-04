0122701b1567c47fd43c32cff9b4bd56ae98fbfe
“iAmash412” <aafthab.ashraff@gmail.com>
2022-06-04 13:52:58 +0100

    removed Signer from app.config

--- motive/__init__.py
+++ motive/__init__.py
@@ -26,7 +26,6 @@ app.config.update(
     
     SESSION_TYPE = "redis",
     SESSION_PERMANENT = False,
-    SESSION_USE_SIGNER = True,
     SESSION_REDIS = redis.from_url("redis://:pd5b772b513d356ff7c0dd1514db19557505e48a390959bd4c93242382ad159ef@ec2-54-194-35-138.eu-west-1.compute.amazonaws.com:31459")
 )
 

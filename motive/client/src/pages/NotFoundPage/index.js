5d9ada45280e403e1952ecbd6c63beb6c851c039
“iAmash412” <aafthab.ashraff@gmail.com>
2022-06-04 21:32:38 +0100

    removed client file

--- motive/client/src/pages/NotFoundPage/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const NotFoundPage = () => {
-    return <h1>Error 404! Page not found</h1>
-}
-
-export default NotFoundPage;
--- motive/client/src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export { default as LandingPage } from './LandingPage';
-export { default as LoginPage } from './LoginPage';
-export { default as RegisterPage } from './RegisterPage';
-export { default as UserPage } from './UserPage';
-export { default as NotFoundPage } from './NotFoundPage';
